b1721b30db9a429dc89857c87b2188088b8ebd9f
shibumi2906 <164049385+shibumi2906@users.noreply.github.com>
2024-05-19 13:29:33 +0300

    +светильники

--- divan/divan/spiders/lighting.py
+++ divan/divan/spiders/lighting.py
@@ -11,33 +11,28 @@ class LightingSpider(scrapy.Spider):
         product_links = response.css('a[href*="/product/"]::attr(href)').getall()
         self.log(f'Found {len(product_links)} product links')
 
-        if not product_links:
-            self.log('No product links found!')
         for link in product_links:
             yield response.follow(link, self.parse_product)
 
-        # Проверяем наличие следующей страницы
         next_page = response.css('a.pagination__next::attr(href)').get()
         if next_page:
             self.log(f'Next page found: {next_page}')
             yield response.follow(next_page, self.parse)
-        else:
-            self.log('No next page link found!')
 
     def parse_product(self, response):
-        # Извлекаем название и цену продукта
         name = response.css('h1[itemprop="name"]::text').get()
         price = response.css('span[itemprop="price"]::attr(content)').get()
-        self.log(f'Parsed product: {name} - {price}')
 
-        if not name or not price:
-            self.log(f'Missing data on {response.url}')
-        yield {
-            'name': name,
-            'price': price,
-            'link': response.url
-        }
+        if name and price:
+            yield {
+                'name': name,
+                'price': price,
+                'link': response.url
+            }
 
+# В settings.py добавьте следующую строку, чтобы сохранить данные в CSV файл:
+FEED_FORMAT = 'csv'
+FEED_URI = 'lighting.csv'
 
 
 
