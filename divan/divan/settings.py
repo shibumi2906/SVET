9999a61cbb3c88767b990a8d925fcb625834d779
shibumi2906 <164049385+shibumi2906@users.noreply.github.com>
2024-08-31 14:14:32 +0300

    story

--- divan/divan/settings.py
+++ divan/divan/settings.py
@@ -91,3 +91,17 @@ ROBOTSTXT_OBEY = True
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+# Настройки проекта Scrapy
+BOT_NAME = 'divan'
+
+SPIDER_MODULES = ['divan.spiders']
+NEWSPIDER_MODULE = 'divan.spiders'
+
+# Подчиняться правилам robots.txt
+ROBOTSTXT_OBEY = True
+
+# Настройки экспорта данных
+FEED_FORMAT = 'csv'
+FEED_URI = 'lighting.csv'
+
+# Другие настройки могут быть добавлены здесь
